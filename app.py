57b92c5396556431f2018451fc6112872ce18290
Armaanoajay <144220308+Armaanoajay@users.noreply.github.com>
2024-11-16 18:13:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -686,7 +686,8 @@ def predict():
     return render_template('main.html', prediction=predicted_class_name, show_disclaimer=False)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host='0.0.0.0', port=port, debug=True)
 
 
 
